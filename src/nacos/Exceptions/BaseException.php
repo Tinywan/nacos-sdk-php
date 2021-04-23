7bedefba0ab2b372f420d36d9a0da801ac1e9560
万少波 <756684177@qq.com>
2021-04-23 10:19:26 +0800

    update Exception

---
+++ src/nacos/Exceptions/BaseException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nacos\Exceptions;
+
+use Throwable;
+
+/**
+ * @link https://www.php.net/manual/en/class.throwable.php
+ * @link https://www.php.net/manual/zh/class.runtimeexception.php
+ */
+class BaseException extends \RuntimeException implements Throwable
+{
+
+}
